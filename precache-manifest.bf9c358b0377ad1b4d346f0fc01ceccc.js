dcd8562ebf5cb83ec4c91b27e25f3940c0504aed
Duy Truong <duytruongh@gmail.com>
2019-07-01 19:53:09 -0700

    Updates

---
+++ precache-manifest.bf9c358b0377ad1b4d346f0fc01ceccc.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9c84b4ec3dc8d6a6d4692df49d258c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff98bf4ae9d6dbaf1cf",
+    "url": "/static/css/main.4b6946ec.chunk.css"
+  },
+  {
+    "revision": "c495442ab22ead5c4193",
+    "url": "/static/js/2.46fe22ce.chunk.js"
+  },
+  {
+    "revision": "fff98bf4ae9d6dbaf1cf",
+    "url": "/static/js/main.8a62a33a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3c697e9291fb1ee62b54ab6d0571272b",
+    "url": "/static/media/DuyTruongResume2019.3c697e92.pdf"
+  },
+  {
+    "revision": "b6cc0d0381087f32499790ea7d8e395b",
+    "url": "/static/media/archive_blog.b6cc0d03.png"
+  },
+  {
+    "revision": "a0d2dfa400baa6942f12c9ed2ab6cd8f",
+    "url": "/static/media/auth1_plant.a0d2dfa4.png"
+  },
+  {
+    "revision": "8124f25501110564bd5e3f31c069452e",
+    "url": "/static/media/auth2_plant.8124f255.png"
+  },
+  {
+    "revision": "c5f2017841c216c8d543bdfddd87c244",
+    "url": "/static/media/body2_golden.c5f20178.png"
+  },
+  {
+    "revision": "cd13acc28fa8f90b38ff6b89f9779887",
+    "url": "/static/media/body2_photo.cd13acc2.png"
+  },
+  {
+    "revision": "bae3fcd9bcccf49f586e4c969b6e99a7",
+    "url": "/static/media/body3_photo.bae3fcd9.png"
+  },
+  {
+    "revision": "eaad201b23a2fa0468bf7acc2aa8fcea",
+    "url": "/static/media/body_golden.eaad201b.png"
+  },
+  {
+    "revision": "371362757c5edee716e6ad797d622fb7",
+    "url": "/static/media/body_photo.37136275.png"
+  },
+  {
+    "revision": "8350f4413c36517a8cb095d27bda0e2f",
+    "url": "/static/media/card_plant.8350f441.png"
+  },
+  {
+    "revision": "b2015ad0dd34c04835f025578402d9c5",
+    "url": "/static/media/cards_plant.b2015ad0.png"
+  },
+  {
+    "revision": "eb603e9ff6908e559614e12814b74b6c",
+    "url": "/static/media/choice_restaurant.eb603e9f.png"
+  },
+  {
+    "revision": "23e644b915e806107fc982a8fcb21b31",
+    "url": "/static/media/contact_photo.23e644b9.png"
+  },
+  {
+    "revision": "5bbbe81a54c7da3d6e6d3762eb327d52",
+    "url": "/static/media/contentful2_blog.5bbbe81a.png"
+  },
+  {
+    "revision": "59423764ea87448480e8508594e1cae3",
+    "url": "/static/media/contentful_blog.59423764.png"
+  },
+  {
+    "revision": "fbd17dd9b437ff6509263fd34ad32d5c",
+    "url": "/static/media/details_film.fbd17dd9.png"
+  },
+  {
+    "revision": "4ee6854ae877fd8ae25af0a14123a338",
+    "url": "/static/media/favorites_film.4ee6854a.png"
+  },
+  {
+    "revision": "0933444ff5d384f1002d10b6d1152e8d",
+    "url": "/static/media/form_plant.0933444f.png"
+  },
+  {
+    "revision": "9c62c08c800b001c009fcd4fd2a5c4b8",
+    "url": "/static/media/home_plant.9c62c08c.png"
+  },
+  {
+    "revision": "14dddcc9dcf55c264c979597feafea60",
+    "url": "/static/media/landing_plant.14dddcc9.png"
+  },
+  {
+    "revision": "69e01caa4766a98c9164e62bcbacf550",
+    "url": "/static/media/logo.69e01caa.png"
+  },
+  {
+    "revision": "cb3b1ec6215dfadbe60652a6e135c173",
+    "url": "/static/media/logo.cb3b1ec6.png"
+  },
+  {
+    "revision": "ceadebc076911557b7e6f9815d7ca63d",
+    "url": "/static/media/logo.ceadebc0.png"
+  },
+  {
+    "revision": "eb6e6b74bee79915e3ed69a1ee5302fb",
+    "url": "/static/media/logo.eb6e6b74.png"
+  },
+  {
+    "revision": "f6c6e6f1b7397107ca0f23ab521adcf0",
+    "url": "/static/media/logout_plant.f6c6e6f1.png"
+  },
+  {
+    "revision": "42ead07a4c8aabe874ab57627d0265c0",
+    "url": "/static/media/map_restaurant.42ead07a.png"
+  },
+  {
+    "revision": "3496fb04511f147918e93d1728ac42e9",
+    "url": "/static/media/mockup_blog.3496fb04.png"
+  },
+  {
+    "revision": "e533fbe22491c4c677b5096b1f29498f",
+    "url": "/static/media/mockup_film.e533fbe2.png"
+  },
+  {
+    "revision": "4d3894dabe0c2e88a0f60e6720cc8b39",
+    "url": "/static/media/mockup_golden.4d3894da.png"
+  },
+  {
+    "revision": "26ad0299c324c3eb75976b4d29660b61",
+    "url": "/static/media/mockup_photo.26ad0299.png"
+  },
+  {
+    "revision": "cf9b65400ee4a7306d5a520389d89565",
+    "url": "/static/media/mockup_plant.cf9b6540.png"
+  },
+  {
+    "revision": "999638c1c11ca19dc810ea4e64436349",
+    "url": "/static/media/mockup_restaurant.999638c1.png"
+  },
+  {
+    "revision": "ae9744837bf2fdf88d75509320317b2d",
+    "url": "/static/media/nav_plant.ae974483.png"
+  },
+  {
+    "revision": "9b48fab9cfb1df5c13ffc969cfd7c16a",
+    "url": "/static/media/phone_restaurant.9b48fab9.png"
+  },
+  {
+    "revision": "581349d59400d5f5dc568e9808489b22",
+    "url": "/static/media/post2_blog.581349d5.png"
+  },
+  {
+    "revision": "96fbb670c71a7c3ca6567bd3117ee8f1",
+    "url": "/static/media/post3_blog.96fbb670.png"
+  },
+  {
+    "revision": "d74b64a6f8dc8910a58cf1d5b4393d7b",
+    "url": "/static/media/post_blog.d74b64a6.png"
+  },
+  {
+    "revision": "608526f49bdcfa49fbf72e54c01a5caa",
+    "url": "/static/media/results_restaurant.608526f4.png"
+  },
+  {
+    "revision": "ec353f8f16aa7f4698bee92c6c8bb7de",
+    "url": "/static/media/trending_film.ec353f8f.png"
+  }
+]);
